353da665e30fc5c20504e4992a4c5797be36021e
norislim <limhuinoris@gmail.com>
2025-07-09 02:24:13 +0800

    PLS WORK

--- marukyu2.py
+++ marukyu2.py
@@ -1,6 +1,17 @@
-import asyncio
+import os, asyncio
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("Start command received")
+    await update.message.reply_text("✅ Bot is working!")
 
 async def main():
-    print("Hello")
+    app = ApplicationBuilder().token("7667049151:AAFCFEeY9FmjDjOlREWLs_ta-Fg5V2TTUJk").build()
+    app.add_handler(CommandHandler("start", start))
+    await app.run_polling()
 
-asyncio.run(main())
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
